7a5fb772f1aed60725157205053d75ec234bae61
Alex_Heiz <sania-l@bk.ru>
2023-03-24 06:57:38 +0300

    last 24.03.23

--- 111.py
+++
@@ -1,7 +0,0 @@
-import telebot
-token='363684825:AAGjj4rdAoDZScibOTIczoe0Uu5QVDvHMWY'
-bot=telebot.TeleBot(token)
-@bot.message_handler(commands=['start'])
-def start_message(message):
-  bot.send_message(message.chat.id,"Привет ✌️ ")
-bot.infinity_polling()
